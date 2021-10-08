038938ad2eb4099965b1256f7369f8ea26068839
EmRyom <61932752+EmRyom@users.noreply.github.com>
2021-10-08 14:20:30 +0200

    broken ReachingDefinition

--- src/ReachingDefinition.purs
+++ src/ReachingDefinition.purs
@@ -16,81 +16,111 @@ rdGenerate (Left err) =
   let pos = showPosition $ parseErrorPosition err in
   "Error: " <> message <> " at " <> pos
 rdGenerate (Right p) = let edges = pgProgram p in case p of 
-  Program d s -> initReachingDefinition edges (defineVariables d Nil) <> (initPG edges)
+  Program d s -> """/*
+""" <> (printReachingDefinitions $ initReachingDefinition edges (defineVariables d Nil)) <> """*/
+""" <> (initPG edges)
 
-
-initReachingDefinition :: List Edge -> List Element -> List (List (Tuple Element (Tuple Int Int)))
+initReachingDefinition :: List Edge -> List Element -> List ReachingDefinition
 initReachingDefinition edges elements = 
   let cs = constraints edges in
   let firstRD = unknownDefinition elements 0 in
   let nextConstraints = findConstraint cs 0 in
+  reachingDefinition firstRD nextConstraints
+
+reachingDefinition :: ReachingDefinition -> List Constraint -> List ReachingDefinition
+reachingDefinition (RD i a) cs = 
+  let n = (applyConstraints (RD i a) (findConstraint cs i)) in
+  mergeRD (rd n cs) n
 
 
-reachingDefinition :: List (Tuple Element (Tuple Int Int)) -> List (Tuple (Tuple Int Int) (Tuple Set Set)) -> List (List (Tuple Element (Tuple Int Int)))
-reachingDefinition previousRD constraints = rd 
+rd :: List ReachingDefinition -> List Constraint -> List ReachingDefinition
+rd (a:as) e = mergeRD (reachingDefinition a e) (rd as e)
+rd Nil _ = Nil
 
 
+mergeRD :: List ReachingDefinition -> List ReachingDefinition -> List ReachingDefinition
+mergeRD (a:as) bs = mergeRD as (a:bs)
+mergeRD Nil a = a
 
-findConstraint :: List (Tuple (Tuple Int Int) (Tuple Set Set)) -> Int -> List (Tuple (Tuple Int Int) (Tuple Set Set))
-findConstraint ((x /\ z) /\ (a /\ b):xs) y = 
+applyConstraints :: ReachingDefinition -> List Constraint -> List ReachingDefinition
+applyConstraints a (b:bs) = (solveConstraint a b:applyConstraints a bs)
+applyConstraints _ Nil = Nil 
+  
+solveConstraint :: ReachingDefinition -> Constraint -> ReachingDefinition
+solveConstraint (RD i a) (C _ o k g) = 
+  case k of 
+  Kill e -> let newAl = removeElement a e in 
+    case g of 
+      Gen x y z -> RD o (A x y z:newAl)
+      NoGen -> RD o newAl
+  NoKill -> case g of 
+    Gen x y z -> RD o (A x y z:a)
+    NoGen -> RD o a 
+
+
+removeElement :: List Assignment -> Element -> List Assignment
+removeElement (A a b c:as) d = 
+  if name a == name d
+  then removeElement as d
+  else (A a b c:removeElement as d)
+removeElement Nil _ = Nil 
+
+findConstraint :: List Constraint -> Int -> List Constraint
+findConstraint (C x z a b:xs) y = 
   if y == x 
-  then ((x /\ z) /\ (a /\ b):findConstraint xs y)
+  then (C x z a b:findConstraint xs y)
   else findConstraint xs y
 findConstraint Nil _ = Nil
 
 
-constraints :: List Edge -> List (Tuple (Tuple Int Int) (Tuple Set Set))
+constraints :: List Edge -> List Constraint
 constraints (e:es) = (constraint e:constraints es)
 constraints Nil = Nil
 
-constraint :: Edge -> Tuple (Tuple Int Int) (Tuple Set Set)
+constraint :: Edge -> Constraint
 constraint (E x y z) = case y of
   D a -> case a of 
-      DVar b -> (x /\ z) /\ (Neither /\ Gen (Var b) x z)
-      DArray b c -> (x /\ z) /\ (Neither /\ Gen (Array b) x z)
-      DRecord b -> (x /\ z) /\ (Neither /\ Gen (Record b) x z) 
-      _ -> (x /\ z) /\ (Neither /\ Neither)
+      DVar b -> C x z NoKill (Gen (Var b) x z)
+      DArray b c -> C x z NoKill $ Gen (Array b) x z
+      DRecord b -> C x z NoKill $ Gen (Record b) x z 
+      _ -> C x z NoKill NoGen
   S a -> case a of 
       LDef b c -> case b of 
-        LVar o -> (x /\ z) /\ (Kill (Var o) /\ Gen (Var o) x z)
-        LArray o _ -> (x /\ z) /\ (Kill (Array o) /\ Gen (Array o) x z)
-        LRfst o -> (x /\ z) /\ (Kill (Record o) /\ Gen (Record o) x z)
-        LRsnd o -> (x /\ z) /\ (Kill (Record o) /\ Gen (Record o) x z) 
-      RDef b c d -> (x /\ z) /\ (Kill (Record b) /\ Gen (Record b) x z) 
+        LVar o -> C x z (Kill (Var o)) $ Gen (Var o) x z
+        LArray o _ -> C x z (Kill (Array o)) $ Gen (Array o) x z
+        LRfst o ->  C x z (Kill (Record o)) $ Gen (Record o) x z
+        LRsnd o ->  C x z (Kill (Record o)) $ Gen (Record o) x z
+      RDef b c d -> C x z (Kill (Record b)) $ Gen (Record b) x z
       Read b -> case b of 
-        LVar o -> (x /\ z) /\ (Neither /\ Gen (Var o) x z)
-        LArray o _ -> (x /\ z) /\ (Neither /\ Gen (Array o) x z)
-        LRfst o -> (x /\ z) /\ (Neither /\ Gen (Record o) x z)
-        LRsnd o -> (x /\ z) /\ (Neither /\ Gen (Record o) x z) 
-      Write b -> (x /\ z) /\ (Neither /\ Neither)
-      _ -> (x /\ z) /\ (Neither /\ Neither)
-  B a -> (x /\ z) /\ (Neither /\ Neither)
+        LVar o ->  C x z (Kill (Var o)) $ Gen (Var o) x z
+        LArray o _ ->  C x z NoKill $ Gen (Array o) x z
+        LRfst o -> C x z (Kill (Record o)) $ Gen (Record o) x z
+        LRsnd o -> C x z (Kill (Record o)) $ Gen (Record o) x z
+      Write b -> C x z NoKill NoGen
+      _ -> C x z NoKill NoGen
+  B a -> C x z NoKill NoGen
    
-unknownDefinition :: List Element -> Int -> List (Tuple Element (Tuple Int Int))
-unknownDefinition (e:es) d = ((e /\ ((-1) /\ d)):unknownDefinition es d)
-unknownDefinition Nil d = Nil
+unknownDefinition :: List Element -> Int -> ReachingDefinition
+unknownDefinition e d = (RD d (ud e d))
+
+ud :: List Element -> Int -> List Assignment
+ud (e:es) d = (A e (-1) d:ud es d)
+ud Nil _ = Nil
 
 
 data ReachingDefinition = RD Int (List Assignment)
                           -- Node|Reaching Definition
 data Assignment         = A Element Int Int
-                         -- Var/Arr|In |Out
-data Constraint         = C Int Int Set Set
-                         -- In |Out|Kill|Gen
+                         -- Var/Arr|Out|In
+data Constraint         = C Int Int Killset Genset
+                         -- Out|In|Kill|Gen
 data Element 
   = Var String 
   | Array String 
   | Record String 
-
-data Set 
-  = Kill Element
-  | Gen Element Int Int
-  | Neither
-  
 data Genset 
   = Gen Element Int Int 
   | NoGen
-
 data Killset 
   = Kill Element
   | NoKill
@@ -129,33 +159,17 @@ name (Var a) = a
 name (Array a) = a
 name (Record a) = a
 
+printReachingDefinitions :: List (ReachingDefinition) -> String
+printReachingDefinitions (RD a b:as) = "RD(q" <> show a <> ")={" <> printRD b <> """}
+""" <> printReachingDefinitions as 
+printReachingDefinitions Nil = ""
 
-{-
-printElement :: List Element -> String
-printElement (Var a:as) = "Var " <> a <> """
-""" <> printElement as
-printElement (Array a:as) = "Array " <> a <> """
-""" <> printElement as
-printElement (Record a:as) = "Record " <> a <> """
-""" <> printElement as
-printElement Nil = ""
-
-printDeclarations :: List Element -> String 
-printDeclarations le = """/*
-""" <> printElement le <> """*/
-"""-}
-
-printReachingDefinitions :: List (List (Tuple Element (Tuple Int Int))) -> Int -> String
-printReachingDefinitions (a:as) e = "RD(q" <> show e <> ")={" <> printRD a <> """}
-""" <> printReachingDefinitions as (e+1)
-printReachingDefinitions Nil _ = ""
-
-printRD :: List (Tuple Element (Tuple Int Int)) -> String 
+printRD :: List (Assignment) -> String 
 printRD (a:as) = "(" <> strRD a <> ")" <> printRD as
 printRD Nil = ""
 
-strRD :: Tuple Element (Tuple Int Int) -> String 
-strRD (e /\ (a /\ b)) = let r = "," <> (if a == -1 then "?" else show a) <> "," <> show b in
+strRD :: Assignment -> String 
+strRD (A e a b) = let r = "," <> (if a == -1 then "?" else show a) <> "," <> show b in
   case e of 
     Var f -> f <> r
     Array f -> f <> r
